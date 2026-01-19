c4e99234e3f391ed9da8b6d8a1f3764e743001e4
georginobouko3 <georginobouko3@gmail.com>
2026-01-19 15:07:01 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,9 +11,30 @@ const roboto = Roboto({
 });
 
 export const metadata: Metadata = {
-  title: "Next.js + Tailwind Portfolio Page",
+  title: " Georgino Bouko - Développeur Web & Mobile",
+  keywords:
+    "Georgino Bouko, Georgino, Bouko, Portfolio, Développeur Web, Développeur Mobile, React, Next.js, Vue.js, JavaScript, TypeScript, HTML, CSS, Tailwind CSS, Material Tailwind, Laravel, PHP, MySQL, MongoDB, Git, GitHub",
+  authors: [{ name: "Georgino Bouko" }],
+  creator: "Georgino Bouko - Développeur Web & Mobile",
   description:
-    "Introducing Tailwind Portfolio Page, a versatile and stylish portfolio template built on the foundation of Tailwind CSS and Material Tailwind.",
+    "Bienvenue sur le portfolio de Georgino Bouko, développeur web et mobile passionné. Découvrez mes projets, compétences et expériences dans le développement d'applications modernes et performantes.",
+  openGraph: {
+    title: "Georgino Bouko - Développeur Web & Mobile",
+    description:
+      "Bienvenue sur le portfolio de Georgino Bouko, développeur web et mobile passionné. Découvrez mes projets, compétences et expériences dans le développement d'applications modernes et performantes.",
+    url: "https://portfolio-mauve-eight-20.vercel.app/",
+    siteName: "Georgino Bouko Portfolio",
+    images: [
+      {
+        url: "/G.png",
+        width: 1200,
+        height: 630,
+        alt: "Georgino Bouko Portfolio",
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -29,7 +50,7 @@ export default function RootLayout({
           data-site="YOUR_DOMAIN_HERE"
           src="https://api.nepcha.com/js/nepcha-analytics.js"
         ></script>
-        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/G.png" type="image/png" />
       </head>
       <body className={roboto.className}>
         <Layout>
