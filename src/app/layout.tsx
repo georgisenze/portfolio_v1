8ebb24361e69ebedb11c389d121e77bbf9953273
georginobouko3 <georginobouko3@gmail.com>
2026-01-21 15:31:37 +0100

    mise à jour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import { Layout, FixedPlugin } from "@/components";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -57,6 +58,7 @@ export default function RootLayout({
         <Layout>
           {children}
           <Analytics />
+          <SpeedInsights />
           <FixedPlugin />
         </Layout>
       </body>
